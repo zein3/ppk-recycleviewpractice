15105d827367bb657417311273cb64a6cb4f0799
zein3 <zeinhaddad02@gmail.com>
2023-11-11 11:25:22 +0700

    kegiatan selesai

---
+++ app/src/main/java/com/polstat/recycleviewpractice/MahasiswaAdapter.kt
@@ -0,0 +1,39 @@
+package com.polstat.recycleviewpractice
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MahasiswaAdapter(private val dataList: Array<Mahasiswa>) :
+    RecyclerView.Adapter<MahasiswaAdapter.MahasiswaViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MahasiswaViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.row_mahasiswa, parent, false)
+        return MahasiswaViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MahasiswaViewHolder, position: Int) {
+        holder.txtNama.text = dataList[position].nama
+        holder.txtNim.text = dataList[position].nim
+        holder.txtNoHp.text = dataList[position].nohp
+    }
+
+    override fun getItemCount() = dataList.size
+
+    class MahasiswaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtNama: TextView
+        val txtNim: TextView
+        val txtNoHp: TextView
+
+        init {
+            txtNama = itemView.findViewById(R.id.txt_nama_mahasiswa)
+            txtNim = itemView.findViewById(R.id.txt_nim_mahasiswa)
+            txtNoHp = itemView.findViewById(R.id.txt_nohp_mahasiswa)
+        }
+    }
+
+}
+
--- app/src/main/java/com/polstat/recycleviewpractice/MainActivity.kt
+++ app/src/main/java/com/polstat/recycleviewpractice/MainActivity.kt
@@ -2,10 +2,39 @@ package com.polstat.recycleviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MahasiswaAdapter
+    private lateinit var mahasiswaList: Array<Mahasiswa>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addData()
+
+        recyclerView = findViewById(R.id.recycler_view)
+        adapter = MahasiswaAdapter(mahasiswaList)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
+    }
+
+    private fun addData() {
+        mahasiswaList = arrayOf(
+            Mahasiswa("Dimas Maulana", "1414370309", "123456789"),
+            Mahasiswa("Fadly Yonk", "1214234560", "987654321"),
+            Mahasiswa("Ariyandi Nugraha", "1214230345", "987648765"),
+            Mahasiswa("Aham Siswana", "1214378098", "098758124"),
+            Mahasiswa("Rahadi Jalu", "235121344", "0812345355"),
+            Mahasiswa("Muhammad Luqman", "123445560", "099877384857"),
+            Mahasiswa("Isfan Fauzi", "7879230345", "548586723"),
+            Mahasiswa("Geri Yesa", "00904378098", "928986872")
+        )
     }
 }
